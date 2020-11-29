6114897dce1c0be07dfa1793bcd7a921675946a0
Elric920 <1871206768@qq.com>
2020-11-29 23:38:57 +0800

    第一次提交

---
+++ Assets/Scripts/MoveManager.cs
@@ -0,0 +1,26 @@
+﻿/***
+ * Title：
+ * Description:
+ * Author：IMDT_Elric
+ * Date:
+ * Version:
+ */
+ 
+using UnityEngine;
+
+public class MoveManager : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(10, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
